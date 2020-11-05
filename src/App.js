2e005f982af0d1726c3e3b27e803ddab72f55f5f
dimassabimanyy <dmsabmny@gmail.com>
2020-11-06 06:48:37 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact={true}>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
