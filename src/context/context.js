fc4cda910611b4a5529e420512498a1270a239e6
dimassabimanyy <dmsabmny@gmail.com>
2020-11-07 04:10:30 +0700

    crete search navbar and requests logic

--- src/context/context.js
+++ src/context/context.js
@@ -1,24 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 
 // Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
 
-    const [githubUser, setGithubUser] = useState(mockUser);
-    const [repos, setRepos] = useState(mockRepos);
-    const [followers, setFollowers] = useState(mockFollowers);
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-    return <GithubContext.Provider value={{ githubUser, repos, followers }}>
-        {children}
+  // error
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          // throw an error
+          toggleError(true, "sorry, you have exceeded your hourly rate limit");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  //error
+  useEffect(checkRequests, []);
+
+  return (
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
+      {children}
     </GithubContext.Provider>
-}
+  );
+};
 
-export { GithubContext, GithubProvider }
+export { GithubContext, GithubProvider };
